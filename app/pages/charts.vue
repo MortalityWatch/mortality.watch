5e02d1de6a080b312180a9e86d23027ca1f6e00a
MortalityWatch <mortalitywatch@protonmail.com>
2025-10-30 16:57:06 -0400

    fix: Use correct component name ChartsChartCard

--- app/pages/charts.vue
+++ app/pages/charts.vue
@@ -54,7 +54,7 @@
       v-else-if="charts && charts.length > 0"
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
     >
-      <ChartCard
+      <ChartsChartCard
         v-for="chart in charts"
         :key="chart.id"
         :chart="chart"
