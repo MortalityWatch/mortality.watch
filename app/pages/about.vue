754e0277dafbe4525b8ad5e34e5b3a05b8c1e450
Mortality Watch <157654555+MortalityWatch@users.noreply.github.com>
2026-02-13 16:24:26 -0500

    update links (#480)
    Co-authored-by: MortalityWatch <mortalitywatch@protonmail.com>

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -238,7 +238,7 @@
         <p class="text-gray-700 dark:text-gray-300">
           Discover more:
           <ULink
-            to="https://old.mortality.watch/charts/list.html"
+            to="https://charts.mortality.watch/"
             target="_blank"
             title="More Charts"
             active-class="text-primary"
@@ -248,7 +248,7 @@
           </ULink>
           ·
           <ULink
-            to="https://old.mortality.watch/data/index.html"
+            to="https://data.mortality.watch/"
             target="_blank"
             title="Chart Data"
             active-class="text-primary"
